b5c96c3228a9ed823d0a992034cd319395da9dea
Raphael Berger <raphaelmb@gmail.com>
2023-01-09 09:50:09 -0300

    Add comment service

---
+++ internal/comment/comment.go
@@ -0,0 +1,59 @@
+package comment
+
+import (
+	"context"
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrFetchingComment = errors.New("failed to fetch comment by id")
+	ErrNotImplemented  = errors.New("not implemented")
+)
+
+// Representation of the comment structure of the service
+type Comment struct {
+	ID     string
+	Slug   string
+	Body   string
+	Author string
+}
+
+// Defines all the methods the service needs in order to operate
+type Store interface {
+	GetComment(context.Context, string) (Comment, error)
+}
+
+// Struct on with all logic will be build on top of
+type Service struct {
+	Store Store
+}
+
+// Returns a pointer to a new service
+func NewService(store Store) *Service {
+	return &Service{
+		Store: store,
+	}
+}
+
+func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
+	fmt.Println("retrieving a comment")
+	cmt, err := s.Store.GetComment(ctx, id)
+	if err != nil {
+		fmt.Println(err)
+		return Comment{}, ErrFetchingComment
+	}
+	return cmt, nil
+}
+
+func (s *Service) UpdateComment(ctx context.Context, cmt Comment) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) DeleteComment(ctx context.Context, id string) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) CreateComment(ctx context.Context, cmt Comment) (Comment, error) {
+	return Comment{}, ErrNotImplemented
+}
