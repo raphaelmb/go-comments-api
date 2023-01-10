fb0748ec2944a39780f2dcdb1bb45bdeab210afe
Raphael Berger <raphaelmb@gmail.com>
2023-01-10 10:55:23 -0300

    Add db connection

---
+++ Makefile
@@ -0,0 +1,13 @@
+build:
+	go build -o app cmd/server/main.go
+
+test:
+	go test -v ./...
+
+lint:
+	golangci-lint run
+
+run:
+	docker compose up --build
+
+.PHONY: build test lint run
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	db "github.com/raphaelmb/go-comments-api/internal/database"
+)
 
 // Responsible for the instantiation and startup of the app
 func Run() error {
 	fmt.Println("Startup...")
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to the database")
+		return err
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+	fmt.Println("successfully connected and pinged database")
 	return nil
 }
 
